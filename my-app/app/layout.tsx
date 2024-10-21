cdf9015e72b7fb70b0efeb734bc211d745157bd9
fardardnsyh <faridardiansyah061@gmail.com>
2024-10-21 13:57:15 +0700

    Files upload

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/react';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Portfolio',
+  description: 'My Web Design UI portfolio using Vercel, React, NextJS, Radix UI & TailwindCSS.',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+          <Analytics />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
